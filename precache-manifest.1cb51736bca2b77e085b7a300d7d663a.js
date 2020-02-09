8215f56d8b9c60089d5108d358f2e51837aa389a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-09 15:30:52 +0000

    Deploy vks-guys/test-app to github.com/vks-guys/test-app.git:gh-pages

---
+++ precache-manifest.1cb51736bca2b77e085b7a300d7d663a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b58207b9c033202912043a4745b0f7b",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "3d4b599c7ae6253c969a",
+    "url": "/test-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d60f1629cfd06c1c48be",
+    "url": "/test-app/static/js/2.6199a9ab.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/test-app/static/js/2.6199a9ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d4b599c7ae6253c969a",
+    "url": "/test-app/static/js/main.3f7f7043.chunk.js"
+  },
+  {
+    "revision": "a7685168259b13e7085a",
+    "url": "/test-app/static/js/runtime-main.350dc61c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
